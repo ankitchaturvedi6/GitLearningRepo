b04fc755d5de5bc9c1749c6faf8bcc18684d7506
ankit <ankitsh123456@gmail.com>
2020-04-06 17:54:47 +0530

    new chenges are made in basics file

--- basics.py
+++ basics.py
@@ -1,3 +1,4 @@
 print("today i am going to learn the python")
 print("but before learning python learn a little about the git hub")
 print("now we are in the dev branch")
+print("new change in dev ")
